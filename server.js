72ead42ee7fb109c150fe0b21be6543e9157b045
mdpfeifer1 <mikelaidenpfeifer@gmail.com>
2023-10-20 17:44:04 -0500

    added comments from readme to give better direction

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// On the back end, the application should include a `db.json` file that will be used to store 
+// and retrieve notes using the `fs` module.
+
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
@@ -8,6 +11,27 @@ const notes = require('./db/db.json')
 
 const PORT = process.env.PORT || 3001;
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
 app.use(express.static('public'));
 
 
+// * `GET /notes` should return the `notes.html` file.
+
+// * `GET *` should return the `index.html` file.
+
+// The following API routes should be created:
+
+// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
+
+// * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` 
+// file, and then return the new note to the client. 
+// You'll need to find a way to give each note a unique id when it's saved 
+// (look into npm packages that could do this for you).
+
+
+
+
+app.listen(PORT, ()  => {
+    console.log(`Server is now on port ${PORT}!`);
+});
