2613558c54a76533ed380edd6659b5d0a807179a
mdpfeifer1 <mikelaidenpfeifer@gmail.com>
2023-10-20 21:05:41 -0500

    bug error on PORT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const routesAPI = require('./routes/routerAPI.js');
 const routesHTML = require('./routes/routerhtml.js');
 const app = express();
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
